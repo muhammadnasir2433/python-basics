dc4dc328154ab4db027fad9e79f56fdb458a2af9
muhammadnasir <muhammadnasir2344@gmail.com>
2025-07-16 20:35:50 +0500

    function by chai awr code

--- 10_class(functions)/class10.py
+++ 10_class(functions)/class10.py
@@ -1,6 +1,14 @@
-from typing import Tuple, Dict
+# from typing import Tuple, Dict
 
-def my_function(a:int,b:int,*abc:int,**xyz:int)->None:
-    print(a,b,abc,xyz)
+# def my_function(a:int,b:int,*abc:int,**xyz:int)->None:
+#     print(a,b,abc,xyz)
 
-my_function(1,2, 2,3,4,5,6,  d = 4,e = 8, c = 80)
+# my_function(1,2, 2,3,4,5,6,  d = 4,e = 8, c = 80)
+
+
+
+import math
+
+def circle_stats(radius):
+    return math.pi * radius ** 2
+print(circle_stats(20))
